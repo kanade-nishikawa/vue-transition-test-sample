c8b2462a7fc65f5f82a0b6f5911726349bf17142
kanade-nishikawa <kanimisounium@gmail.com>
2021-09-18 04:10:25 +0900

    画面遷移のサンプルテストコード

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,41 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mount, flushPromises } from "@vue/test-utils";
+import router from "@/router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import { defineComponent } from "@vue/runtime-dom";
+
+// 確認用の各画面のWrapper
+const homeWrapper = mount(Home, {
+  global: {
+    plugins: [router],
+  },
+});
+const aboutWrapper = mount(About);
 
 describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+  it("Home.vueからAbout.vueへの遷移", async () => {
+    // wrapperにHome.vueを表示させる（初期位置の設定）
+    router.push("/");
+    await router.isReady();
+
+    // 実際にテストで動かす画面を作成
+    const testApp = defineComponent({
+      template: `<router-view />`,
     });
-    expect(wrapper.text()).toMatch(msg);
+    const wrapper = mount(testApp, {
+      global: {
+        plugins: [router],
+      },
+    });
+
+    // 遷移前の画面のHTMLがHome.vueと同一か確認
+    expect(wrapper.html()).toBe(homeWrapper.html());
+
+    // <a>タグ（今回は<router-link>しかない）を取得し、押下処理
+    await wrapper.find("[data-test='router']").trigger("click");
+    await flushPromises();
+
+    // 遷移後の画面のHTMLがAbout.vueと同一か確認
+    expect(wrapper.html()).toBe(aboutWrapper.html());
   });
 });
